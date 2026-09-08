f9aee0a8c0fa7f362c3d5e5080e3e84b4d8a3978
Taron331 <taron2016@gmail.com>
2026-09-09 00:41:58 +0200

    main.py

--- main.py
+++ main.py
@@ -85,4 +85,4 @@ if __name__ == "__main__":
     app = ApplicationBuilder().token(TOKEN).build()
     app.add_handler(CommandHandler("start", start))
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
-    app.run_polling()
+    app.run_polling(drop_pending_updates=True)
